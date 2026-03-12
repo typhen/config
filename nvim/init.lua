76262f114ff83a4e6023d5a6dd4209902a0bd410
Typhen <typhen@posteo.de>
2026-03-12 08:50:27 +0100

    Create nvim/init.lua
    added initial nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- Basic settings
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.smartindent = true
+vim.o.wrap = true -- Enable linewrapping
+vim.o.cursorline = true -- Highligt current line
+vim.o.termguicolors = true
